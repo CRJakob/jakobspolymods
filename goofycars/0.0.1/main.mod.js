61ef5919078da569dbe7e665cee28e63b69a86ca
CR Jakob <78952659+CRJakob@users.noreply.github.com>
2025-04-19 00:13:44 +0200

    idek

--- goofycars/0.0.1/main.mod.js
+++
@@ -1,7 +0,0 @@
-import { PolyMod } from "https://pml.orangy.cfd/0rangy/PolyModLoader/<target>/PolyModLoader.js";
-class YourModClass extends PolyMod {
-
-}
-
-
-export let polyMod = new YourModClass();
