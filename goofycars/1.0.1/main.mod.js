cff37d3de15ab45cc9ba464f2e212de1234f1216
CR Jakob <78952659+CRJakob@users.noreply.github.com>
2025-04-19 01:53:10 +0200

    test mod js

--- goofycars/1.0.1/main.mod.js
+++ goofycars/1.0.1/main.mod.js
@@ -1,6 +1,11 @@
 import { PolyMod, MixinType } from "https://cdn.jsdelivr.net/gh/0rangy/PolyModLoader@0.5.0/PolyModLoader.js";
 
-class OrangysCarSwitcherMod extends PolyMod {
+class goofycars extends PolyMod {
+	dependencyInit = (OrangysCarSwitcherMod) => {
+		OrangysCarSwitcherMod.carmodels = [
+			
+		]
+	}
 }
 
-export let polyMod = new OrangysCarSwitcherMod();
+export let polyMod = new goofycars();
