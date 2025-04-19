f7fd6e541a58ed67350332e4abc4509104e2a31a
CR Jakob <78952659+CRJakob@users.noreply.github.com>
2025-04-19 16:58:46 +0200

    dumbahh mistake

--- goofycars/1.0.1/main.mod.js
+++ goofycars/1.0.1/main.mod.js
@@ -1,5 +1,5 @@
 import { PolyMod, MixinType } from "https://pml.orangy.cfd/PolyTrackMods/PolyModLoader/0.5.0/PolyModLoader.js";
 
-class goofycars extends PolyMod {};
+class goofycars extends PolyMod {}
 
 export let polyMod = new goofycars();
