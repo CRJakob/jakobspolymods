1a0037df5d8233280319966008008660de39ae3f
CR Jakob <78952659+CRJakob@users.noreply.github.com>
2025-04-20 22:52:32 +0200

    Update main.mod.js

--- goofycars/1.0.1/main.mod.js
+++ goofycars/1.0.1/main.mod.js
@@ -1,17 +1,20 @@
-import { PolyMod, MixinType } from "https://pml.orangy.cfd/PolyTrackMods/PolyModLoader/0.5.0/PolyModLoader.js";
+// before (doesn't match CarSwitcher):
+// import { PolyMod } from "https://pml.orangy.cfd/PolyTrackMods/PolyModLoader/0.5.0/PolyModLoader.js";
 
-class goofycars extends PolyMod {
-    dependencyInit(mod) {
-        if(mod.id == 'carswitcher') {
-            alert(mod.carModels)
-            mod.carModels.push(
-                {
-                    "name": "Poly Huracan",
-                    "url": `${this.modBaseUrl}/${this.modVersion}/assets/huracan.glb`
-                }
-            )
-        }
+// after (identical to carswitcher):
+import { PolyMod } from "https://cdn.jsdelivr.net/gh/0rangy/PolyModLoader@0.5.0/PolyModLoader.js";
+
+class GoofyCars extends PolyMod {
+  dependencyInit(mod) {
+    if (mod.id === 'carswitcher') {
+      // now this will actually run
+      alert(mod.carModels);
+      mod.carModels.push({
+        name: "Poly Huracan",
+        url: `${this.modBaseUrl}/${this.modVersion}/assets/huracan.glb`
+      });
     }
+  }
 }
 
-export let polyMod = new goofycars();
+export let polyMod = new GoofyCars();
