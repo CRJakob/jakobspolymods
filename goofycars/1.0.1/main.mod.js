6855ca4956f02fb14a83a0a63fed3f9c0ef619be
CR Jakob <78952659+CRJakob@users.noreply.github.com>
2025-04-20 16:18:33 +0200

    add first car

--- goofycars/1.0.1/main.mod.js
+++ goofycars/1.0.1/main.mod.js
@@ -1,5 +1,10 @@
 import { PolyMod, MixinType } from "https://pml.orangy.cfd/PolyTrackMods/PolyModLoader/0.5.0/PolyModLoader.js";
 
-class goofycars extends PolyMod {}
+class goofycars extends PolyMod {
+    OrangysCarSwitcherMod.carList.push({
+        "name": "Poly Huracan",
+        "url": `${this.modBaseUrl}/${this.modVersion}/assets/huracan.glb`,
+    });
+}
 
 export let polyMod = new goofycars();
