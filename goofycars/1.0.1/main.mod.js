e86221e70eec6bd077de3c3ad8dd27d4eedc7f8e
CR Jakob <78952659+CRJakob@users.noreply.github.com>
2025-04-21 01:05:14 +0200

    update main.mod.js

--- goofycars/1.0.1/main.mod.js
+++ goofycars/1.0.1/main.mod.js
@@ -2,16 +2,12 @@
 import { PolyMod } from "https://cdn.jsdelivr.net/gh/0rangy/PolyModLoader@0.5.0/PolyModLoader.js";
 
 class GoofyCars extends PolyMod {
-  dependencyInit(mod) {
-    if (mod.id == 'carswitcher') {
-      // now this will actually run
-      console.log(mod.carModels);
-      mod.carModels.push({
-        name: "Poly Huracan",
-        url: `${this.modBaseUrl}/${this.modVersion}/assets/huracan.glb`
-      });
-    }
-  }
+    pml.getMod('carswitcher').carModels.push(
+        {
+            "name": "poly huracan"
+            "url": `${this.modBaseUrl}/${this.modVersion}/assets/huracan.glb`
+        }
+    )
 }
 
 export let polyMod = new GoofyCars();
