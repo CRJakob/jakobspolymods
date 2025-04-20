b1962fdcd294e7c4aab2edd0e4a969b690ff2312
CR Jakob <78952659+CRJakob@users.noreply.github.com>
2025-04-20 20:43:02 +0200

    uihj

--- goofycars/1.0.1/main.mod.js
+++ goofycars/1.0.1/main.mod.js
@@ -1,11 +1,14 @@
 import { PolyMod, MixinType } from "https://pml.orangy.cfd/PolyTrackMods/PolyModLoader/0.5.0/PolyModLoader.js";
 
 class goofycars extends PolyMod {
-    dependencyInit("carswitcher");
-    OrangysCarSwitcherMod.carList.push({
-        "name": "Poly Huracan",
-        "url": `${this.modBaseUrl}/${this.modVersion}/assets/huracan.glb`
-    });
+    dependencyinit(mod) {
+        if(mod.id == 'carswitcher') {
+            mod.carList.push({
+                "name": "Poly Huracan",
+                "url": `${this.modBaseUrl}/${this.modVersion}/assets/huracan.glb`
+            })
+        }
+    }
 }
 
 export let polyMod = new goofycars();
