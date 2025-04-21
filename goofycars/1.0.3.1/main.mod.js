a6bf27dac2b4d60676f79c34df6730318c1cd9a1
CR Jakob <78952659+CRJakob@users.noreply.github.com>
2025-04-22 00:19:36 +0200

    update 1.0.3.1

---
+++ goofycars/1.0.3.1/main.mod.js
@@ -0,0 +1,19 @@
+// after (identical to carswitcher):
+import { PolyMod } from "https://cdn.jsdelivr.net/gh/0rangy/PolyModLoader@0.5.0/PolyModLoader.js";
+
+class goofycars extends PolyMod {
+    init = (pml) => {
+        pml.getMod("carswitcher").carModels.push(
+            {
+                "name": "Dababy Car",
+                "url": `${this.modBaseUrl}/${this.modVersion}/assets/dababy.glb`
+            },
+            {
+                "name": "Hitbox Car",
+                "url": `${this.modBaseUrl}/${this.modVersion}/assets/hitbox.glb`
+            }
+        )
+    }
+}
+
+export let polyMod = new goofycars();
