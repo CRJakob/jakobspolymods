235e3ea19e5ae09f0d42eb5502046cd4cf4a58cd
CR Jakob <78952659+CRJakob@users.noreply.github.com>
2025-04-22 22:35:57 +0200

    add coolcars mod

---
+++ coolcars/1.0.1/main.mod.js
@@ -0,0 +1,43 @@
+// after (identical to carswitcher):
+import { PolyMod } from "https://cdn.jsdelivr.net/gh/0rangy/PolyModLoader@0.5.0/PolyModLoader.js";
+
+class goofycars extends PolyMod {
+    init = (pml) => {
+        pml.getMod("carswitcher").carModels.push(
+            {
+                "name": "Dababy Car",
+                "url": `${this.modBaseUrl}/${this.modVersion}/assets/dababy.glb`
+            },
+            {
+                "name": "Hitbox Car",
+                "url": `${this.modBaseUrl}/${this.modVersion}/assets/hitbox.glb`
+            },
+            {
+                "name": "Audi R8",
+                "url": `${this.modBaseUrl}/${this.modVersion}/assets/audir8.glb`
+            },
+            {
+                "name": "Countach",
+                "url": `${this.modBaseUrl}/${this.modVersion}/assets/countach.glb`
+            },
+            {
+                "name": "Koenigsegg",
+                "url": `${this.modBaseUrl}/${this.modVersion}/assets/koenigsegg.glb`
+            },
+            {
+                "name": "SVJ",
+                "url": `${this.modBaseUrl}/${this.modVersion}/assets/svj.glb`
+            },
+            {
+                "name": "Mclaren F1",
+                "url": `${this.modBaseUrl}/${this.modVersion}/assets/mclaren.glb`
+            },
+            {
+                "name": "Nissan Skyline GTR",
+                "url": `${this.modBaseUrl}/${this.modVersion}/assets/nissan.glb`
+            }
+        )
+    }
+}
+
+export let polyMod = new goofycars();
