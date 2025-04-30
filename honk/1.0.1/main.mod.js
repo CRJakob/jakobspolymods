01ec47c7b522f9697f8a6d21a9fc6a5684571428
CR Jakob <78952659+CRJakob@users.noreply.github.com>
2025-04-30 23:47:10 +0200

    Update main.mod.js

--- honk/1.0.1/main.mod.js
+++ honk/1.0.1/main.mod.js
@@ -3,10 +3,14 @@ import { PolyMod, MixinType } from "https://pml.orangy.cfd/PolyTrackMods/PolyMod
 class honkmod extends PolyMod {
     init = (pml) => {
         this.pml = pml;
-        var honk = new Audio(`${this.modBaseUrl}/${this.modVersion}/assets/honk.mp3`);
-        //honk.volume = pml.SoundEffectVolume;
+    }
+    postInit = (pml) => {
+        this.pml = pml;
+        pml.soundManager.registerSound("honk", `${this.modBaseUrl}/${this.modVersion}/assets/honk.flac`);
+        this.audioVolume = pml.getSetting(SoundEffectVolume);
+        honk.volume = this.audioVolume;
         pml.registerBindCategory("Honk Mod");
-        pml.registerKeybind("Honk", "honk", "keydown", "KeyI", null, function(h) {console.log(pml.SoundEffectVolume);});
+        pml.registerKeybind("Honk", "honk", "keydown", "KeyI", null, function(h) {pml.soundManager.playSound("honk", this.audioVolume)});
     }
 }
 
