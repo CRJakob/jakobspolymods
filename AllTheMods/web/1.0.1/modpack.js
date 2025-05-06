68be838322c632aced8ff42aef8a519781dbb700
CR Jakob <78952659+CRJakob@users.noreply.github.com>
2025-05-06 09:42:58 +0200

    a

--- AllTheMods/web/1.0.1/modpack.js
+++ AllTheMods/web/1.0.1/modpack.js
@@ -6,7 +6,7 @@ class allthemodsweb extends PolyMod {
 
         // Mod List
         this.modList = [
-            { url: "https://pml.orangy.cfd/GameBuilder202/ErrorPopupMod/main",         version: "latest" },
+            { url: "https://pml.orangy.cfd/GameBuilder202/ErrorPopupMod/main",        version: "latest" },
             { url: "https://pml.orangy.cfd/0rangy/OrangysPolyMods/main/polyproxy",    version: "latest" },
             { url: "https://pml.orangy.cfd/0rangy/OrangysPolyMods/main/3decspeed",    version: "latest" },
             { url: "https://pml.orangy.cfd/0rangy/OrangysPolyMods/main/husplits",     version: "latest" },
