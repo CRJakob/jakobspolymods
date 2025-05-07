d6d3627c7d64787f6b3d5b9625d7519163105180
CR Jakob <78952659+CRJakob@users.noreply.github.com>
2025-05-07 20:08:30 +0200

    update for more modular approach

--- AllTheMods/web/1.0.1/modpack.js
+++ AllTheMods/web/1.0.1/modpack.js
@@ -5,31 +5,32 @@ class allthemodsweb extends PolyMod {
         this.pml = pml;
         // Mod List
         this.modList = [
-            { url: "https://pml.orangy.cfd/GameBuilder202/ErrorPopupMod/main",        version: "latest" },
-            { url: "https://pml.orangy.cfd/0rangy/OrangysPolyMods/main/polyproxy",    version: "latest" },
-            { url: "https://pml.orangy.cfd/0rangy/OrangysPolyMods/main/3decspeed",    version: "latest" },
-            { url: "https://pml.orangy.cfd/0rangy/OrangysPolyMods/main/husplits",     version: "latest" },
-            { url: "https://pml.orangy.cfd/0rangy/OrangysPolyMods/main/ghosttoggle",  version: "latest" },
-            { url: "https://pml.orangy.cfd/0rangy/OrangysPolyMods/main/carswitcher",  version: "latest" },
-            { url: "https://pml.orangy.cfd/CRJakob/jakobspolymods/main/goofycars/",   version: "latest" },
-            { url: "https://pml.orangy.cfd/CRJakob/jakobspolymods/main/coolcars/",    version: "latest" }
+            { url: "https://pml.orangy.cfd/GameBuilder202/ErrorPopupMod/main", version: "latest" },
+            { url: "https://pml.orangy.cfd/0rangy/OrangysPolyMods/main/polyproxy", version: "latest" },
+            { url: "https://pml.orangy.cfd/0rangy/OrangysPolyMods/main/3decspeed", version: "latest" },
+            { url: "https://pml.orangy.cfd/0rangy/OrangysPolyMods/main/husplits", version: "latest" },
+            { url: "https://pml.orangy.cfd/0rangy/OrangysPolyMods/main/ghosttoggle", version: "latest" },
+            { url: "https://pml.orangy.cfd/0rangy/OrangysPolyMods/main/carswitcher", version: "latest" },
+            { url: "https://pml.orangy.cfd/CRJakob/jakobspolymods/main/goofycars/", version: "latest" },
+            { url: "https://pml.orangy.cfd/CRJakob/jakobspolymods/main/coolcars/", version: "latest" }
         ];
         console.log(this.modList);
         let allloadedmods = pml.getAllMods();
         console.log(allloadedmods);
-        //import and load mods
-        this.modList.forEach(({ url, version }) => {
-            // add mod and return id
-            pml.addMod({
-                base:   url,
-                version: version,
-                loaded: false
-            }).then((mod) => pml.setModLoaded(mod, true));
 
-            // load mods
-            //console.log(addedMod);
-            //window.polyModLoader.setModLoaded(addedMod, true);
-        });
+        // function for loading mods
+        function importMod({ url: modurl, version: modversion }) {
+            window.polyModLoader.addMod({
+                base:    modurl,
+                version: modversion,
+                loaded:  true
+            }).then((mod) => pml.setModLoaded(mod, true));
+        }
+        // import and load mods
+        this.modList.forEach(({ url, version }) => {importMod({url, version})});
+
+        // unload Modpack after imports to prevent duplicate mod errors
+        pml.setModLoaded(this, false);
     }
 }
 
