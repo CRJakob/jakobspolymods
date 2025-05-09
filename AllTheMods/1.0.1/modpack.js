d7c39d1c2918e65781d5a07f83042ddd3a66736f
CR Jakob <78952659+CRJakob@users.noreply.github.com>
2025-05-09 15:09:11 +0200

    unexternalise functions

--- AllTheMods/1.0.1/modpack.js
+++ AllTheMods/1.0.1/modpack.js
@@ -14,6 +14,60 @@ class allthemods extends PolyMod {
       { url: "https://pml.orangy.cfd/CRJakob/jakobspolymods/main/coolcars/", version: "latest" }
     ];
     this.polyProxy = { url: "https://pml.orangy.cfd/0rangy/OrangysPolyMods/main/polyproxy", version: "latest" };
+
+    // function to detect if running on app version
+function isElectron() {
+    // Renderer process
+    if (typeof window !== 'undefined' && typeof window.process === 'object' && window.process.type === 'renderer') {
+        return true;
+    }
+
+    // Main process
+    if (typeof process !== 'undefined' && typeof process.versions === 'object' && !!process.versions.electron) {
+        return true;
+    }
+
+    // Detect the user agent when the `nodeIntegration` option is set to false
+    if (typeof navigator === 'object' && typeof navigator.userAgent === 'string' && navigator.userAgent.indexOf('Electron') >= 0) {
+        return true;
+    }
+
+    return false;
+}
+
+// normalize url function
+function normalize(u) {
+    return new URL(u).href.replace(/\/+$/, "");
+}
+
+function importPolyMod({ url: modurl, version: modversion }) {
+    console.info(`⏳ Attempting to import mod: ${modurl}@${modversion}`);
+
+    // get polyMods object
+    const raw = window.localStorage.getItem("polyMods") || "[]";
+
+    // normalize url
+    const normUrl = normalize(modurl);
+
+    // avoid duplicate mod error
+    if (raw.includes(normUrl)) {
+        // skip
+        console.warn(`⚠️  Skipping import; already in polyMods: ${modurl}`);
+    }
+    // import mod if not already present
+    else {
+        // import mod
+        pml.addMod({ base: modurl, version: modversion, loaded: true })
+        .then(mod => {
+            pml.setModLoaded(mod, true);
+            console.info(`✅ Successfully imported: ${modurl}`);
+        })
+        .catch(err => {
+            console.error(`❌ Failed to import ${modurl}:`, err);
+        });
+    }
+}
+
     let runningElectron = isElectron();
     if(this.includeProxy === true && runningElectron === false) {
         console.info("Running web client, loading PolyProxy")
