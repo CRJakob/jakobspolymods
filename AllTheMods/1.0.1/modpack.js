085f9d487bc488dfeeadc8935bf89aefafe9f49b
CR Jakob <78952659+CRJakob@users.noreply.github.com>
2025-05-09 12:12:18 +0200

    more formatting

--- AllTheMods/1.0.1/modpack.js
+++ AllTheMods/1.0.1/modpack.js
@@ -1,16 +1,9 @@
 import { PolyMod } from "https://pml.orangy.cfd/PolyTrackMods/PolyModLoader/0.5.0/PolyModLoader.js";
 import { normalize, importPolyMod, isElectron } from "https://pml.orangy.cfd/CRJakob/jakobspolymods/main/modpackTools.js";
-
 class allthemods extends PolyMod {
   init = (pml) => {
-    // do not touch this
     this.pml = pml;
-
-    // include the polyProxy mod, true/false
-    this.includeProxy = true;
-
-    // Mod List
-    // { url: "the mod's url", version: "the mod version, use latest for newest mod version" } 
+    this.includeProxy = true; 
     this.modList = [
       { url: "https://pml.orangy.cfd/GameBuilder202/ErrorPopupMod/main", version: "latest" },
       { url: "https://pml.orangy.cfd/0rangy/OrangysPolyMods/main/3decspeed", version: "latest" },
@@ -20,26 +13,15 @@ class allthemods extends PolyMod {
       { url: "https://pml.orangy.cfd/CRJakob/jakobspolymods/main/goofycars/", version: "latest" },
       { url: "https://pml.orangy.cfd/CRJakob/jakobspolymods/main/coolcars/", version: "latest" }
     ];
-
-
-
-    // PolyProxy mod
     this.polyProxy = { url: "https://pml.orangy.cfd/0rangy/OrangysPolyMods/main/polyproxy", version: "latest" };
-    
-    // run Electron check
     let runningElectron = isElectron();
-
-    // install polyProxy mod if on Browser
     if(this.includeProxy === true && runningElectron === false) {
         console.info("Running web client, loading PolyProxy")
         importPolyMod(this.polyProxy)
     } else {
         console.info("Running Electron client, skipping PolyProxy")
     }
-
-    // import and load mod list
     this.modList.forEach(({ url, version }) => {importPolyMod({url, version})});
   }
 }
-
 export let polyMod = new allthemods();
--- modpackTools.js
+++ modpackTools.js
@@ -1,3 +1,4 @@
+// function to detect if running on app version
 function isElectron() {
     // Renderer process
     if (typeof window !== 'undefined' && typeof window.process === 'object' && window.process.type === 'renderer') {
@@ -17,6 +18,7 @@ function isElectron() {
     return false;
 }
 
+// normalize url function
 function normalize(u) {
     return new URL(u).href.replace(/\/+$/, "");
 }
@@ -27,12 +29,15 @@ function importPolyMod({ url: modurl, version: modversion }) {
     // get polyMods object
     const raw = window.localStorage.getItem("polyMods") || "[]";
 
+    // normalize url
     const normUrl = normalize(modurl);
 
+    // avoid duplicate mod error
     if (raw.includes(normUrl)) {
         // skip
         console.warn(`⚠️  Skipping import; already in polyMods: ${modurl}`);
     }
+    // import mod if not already present
     else {
         // import mod
         pml.addMod({ base: modurl, version: modversion, loaded: true })
