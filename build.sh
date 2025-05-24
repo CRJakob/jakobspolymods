ecbdf6ebdfe00566929b4ef57490f3a668eb26aa
CR Jakob <78952659+CRJakob@users.noreply.github.com>
2025-05-24 13:58:03 +0000

    idk

--- build.sh
+++ build.sh
@@ -1,1 +1,8 @@
 tsc
+
+if [[ "$1" == "host" ]]; then
+    trap "trap - TERM && kill -- -$$" INT TERM EXIT
+    (python -m http.server 8001 &> /dev/null) &
+    (cd PolyModLoader; python -m http.server 8000 &> /dev/null) &
+    wait
+fi
